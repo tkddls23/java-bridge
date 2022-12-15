5d5d864bdf1a9516774327a2535fc1138b4bab38
wonseok <tkddls23@konkuk.ac.kr>
2022-12-16 00:26:40 +0900

    feat(view: InputView): 게임 재시작 여부를 입력받는다. (재시도: R, 종료: Q)

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,8 +3,7 @@ package bridge.view;
 import bridge.validation.InputViewValidation;
 import camp.nextstep.edu.missionutils.Console;
 
-import static bridge.constant.MessageConstant.MESSAGE_INPUT_BRIDGE_SIZE;
-import static bridge.constant.MessageConstant.MESSAGE_INPUT_BRIDGE_STEP;
+import static bridge.constant.MessageConstant.*;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -39,6 +38,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(MESSAGE_INPUT_EXIT);
+        String gameCommand = Console.readLine();
+        return gameCommand;
     }
 }
