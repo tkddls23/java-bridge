686cbb347a9fad34b12ed178ed440fac677b948b
wonseok <tkddls23@konkuk.ac.kr>
2022-12-16 00:31:51 +0900

    feat(view: OutputView): 게임 시작 문구를 출력한다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,18 @@
 package bridge.view;
 
+import static bridge.constant.MessageConstant.MESSAGE_OUTPUT_START;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    /**
+     * 게임 시작 문구를 출력한다.
+     */
+    public void printStart() {
+        System.out.println(MESSAGE_OUTPUT_START);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
