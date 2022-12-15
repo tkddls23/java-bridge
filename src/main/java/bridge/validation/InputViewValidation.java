5f09cdff6ed52907b6309560772d9e21899c659c
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 22:59:04 +0900

    feat(validation: InputViewValidation): 입력이 숫자인지 검증 기능 구현

--- src/main/java/bridge/validation/InputViewValidation.java
+++ src/main/java/bridge/validation/InputViewValidation.java
@@ -3,12 +3,25 @@ package bridge.validation;
 import java.util.Objects;
 
 import static bridge.constant.ErrorConstant.ERROR_BLANK_INPUT;
+import static bridge.constant.ErrorConstant.ERROR_NON_INTEGER;
 
 public class InputViewValidation {
 
-    public static void checkInputBlank(String input) {
+    public static void checkInputValid(String input) {
+        checkInputBlank(input);
+        checkInputDigit(input);
+    }
+
+    private static void checkInputBlank(String input) {
         if (Objects.isNull(input) || input.isEmpty()) {
             throw new IllegalArgumentException(ERROR_BLANK_INPUT);
+        }
+    }
+
+    private static void checkInputDigit(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i)))
+                throw new IllegalArgumentException(ERROR_NON_INTEGER);
         }
     }
 }
